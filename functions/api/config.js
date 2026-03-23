66655709a3aec3f9879b261d101264c667d284de
Worklaf <nikolfar91@gmail.com>
2026-03-23 10:14:38 +0100

    Update config.js

--- functions/api/config.js
+++ functions/api/config.js
@@ -15,21 +15,22 @@ export async function onRequest(context) {
     return new Response(null, { headers: corsHeaders });
   }
 
-  // Возвращаем Environment Variables (без секретов!)
+  // Возвращаем Environment Variables (включая API ключ!)
   const config = {
     CF_AUTH_DOMAIN: env.CF_AUTH_DOMAIN || "testnet-hub.firebaseapp.com",
     CF_PROJECT_ID: env.CF_PROJECT_ID || "testnet-hub",
     CF_STORAGE_BUCKET: env.CF_STORAGE_BUCKET || "testnet-hub.firebasestorage.app",
     CF_MESSAGING_SENDER_ID: env.CF_MESSAGING_SENDER_ID || "497813176653",
     CF_APP_ID: env.CF_APP_ID || "1:497813176653:web:089188fdd1555d76cd7704",
-    // API ключ не передаем в открытом API!
-    hasApiKey: !!env.CF_API_KEY,
+    // Передаем настоящий API ключ!
+    CF_API_KEY: env.CF_API_KEY || null,
     ADMIN_UID: env.ADMIN_UID || "SAkz4mdW9reDaIsvqigCNZhEKJR2",
     // Отладка
     debug: {
       envKeys: Object.keys(env || {}),
       hasCF_API_KEY: !!env.CF_API_KEY,
-      hasADMIN_UID: !!env.ADMIN_UID
+      hasADMIN_UID: !!env.ADMIN_UID,
+      apiKeyLength: env.CF_API_KEY ? env.CF_API_KEY.length : 0
     }
   };
 
